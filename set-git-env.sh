4bd2a464dbe2591fbbc1eb2c28d76d372be066b0
raychang <raychang@raychangde-MacBook-Pro.local>
2020-07-14 13:45:34 +0800

    version 0714 | 2020-07-14

--- set-git-env.sh
+++ set-git-env.sh
@@ -15,19 +15,18 @@ apt install silversearcher-ag
 tar zcf bakdata-${today}.tgz face_server/face_image* face_server/datadir
 
 mkdir -p data/face data/food
-cd data/face; mv ../../face_server/face_image* .; mv ../../face_server/models .; cd -
+cd data/face; mv ../../face_server/face_image* .; mv ../../face_server/models .; mv ../../face_server/face_server.ini .; cd -
 cd data; mv ../face_server/datadir .; cd -
 
 cd face_server; ./down.sh; cd -;
 mv face_server face_server_tmp 
 git clone git@github.com:MAAICV/face_server.git
-cd face_server; mv ../face_server_tmp/face_server.ini .;
-icdiff face_server.ini face_server.ini_
-./config.py
-rm face_server.ini_
-cd -
-rm -r face_server_tmp
+icdiff data/face/face_server.ini face_server/face_server.ini_
+./config.py data/face/face_server.ini face_server/face_server.ini_ 
+rm face_server/face_server.ini_
 
+tar zcf bakcode-${today}.tgz face_server_tmp
+rm -r face_server_tmp
 
 # cd data/food; mv ../../food_server/debug_detected_images .; mv ../../food_server/food_data .; mv ../../food_server/models .; cd -
 
